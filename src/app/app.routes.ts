3d0cdd62c3366f1e2f7352843b5e04ce667dae14
ayyooba <ayyooba@fortude.co>
2025-04-14 13:12:54 +0530

    Init: AWS-Cognito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'sign-in', component: SigninComponent },
+    { path: 'sign-up', component: SignupComponent },
+    { path: 'profile', component: ProfileComponent }
+];
