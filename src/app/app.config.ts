314e699dadc8e62f95873fb0fdd99143e91d3f51
ayyooba <ayyooba@fortude.co>
2025-04-27 18:01:26 +0530

    Update: configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from 'angular-auth-oidc-client';
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -12,5 +13,13 @@ import { provideHttpClient } from '@angular/common/http';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), importProvidersFrom(AuthModule.forRoot({
+    config: {
+      authority: 'https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_pj3QdG6s4',
+      redirectUrl: 'http://localhost:4200/sign-in',
+      clientId: 'ao9865cisfb0c1koh5ak1h1lc',
+      scope: 'phone openid email',
+      responseType: 'code'
+    }
+  }))]
 };
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,34 +1,92 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
+  standalone: true,
   imports: [
     CommonModule,
     NzFormModule,
-    FormsModule,
     ReactiveFormsModule,
     NzInputModule,
-    NzButtonModule
+    NzButtonModule,
   ],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
   signinForm: FormGroup;
+  isAuthenticated = false;
+  accessToken = '';
+  userData = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private oidcSecurityService: OidcSecurityService,
+    private httpClient: HttpClient
+  ) {
     this.signinForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    // Subscribe to authentication state
+    this.oidcSecurityService.isAuthenticated$.subscribe(({ isAuthenticated }) => {
+      this.isAuthenticated = isAuthenticated;
+      console.warn('Authenticated:', isAuthenticated);
+
+      if (isAuthenticated) {
+        this.getTokens();
+      }
     });
   }
 
   onSubmit(): void {
-    console.log(this.signinForm.value);
+    // Trigger OIDC login
+    this.oidcSecurityService.authorize();
+    this.getTokens();
+  }
+
+  private getTokens(): void {
+    this.oidcSecurityService.getAccessToken().subscribe((token) => {
+      this.accessToken = token;
+      console.log('Access Token:', this.accessToken);
+  
+      // Now you can call the protected API after getting the token
+      this.callProtectedApi();
+    });
+  
+    this.oidcSecurityService.getIdToken().subscribe((idToken) => {
+      console.log('ID Token:', idToken);
+    });
+  }
+  
+
+  private callProtectedApi(): void {
+    if (!this.accessToken) {
+      console.error('Access Token is not available.');
+      return;
+    }
+
+    this.httpClient.get('https://ap-southeast-1pj3qdg6s4.auth.ap-southeast-1.amazoncognito.com/oauth2/token', {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`
+      }
+    }).subscribe({
+      next: (response) => {
+        console.log('Protected API Response:', response);
+      },
+      error: (error) => {
+        console.error('Error calling protected API:', error);
+      }
+    });
   }
 }
